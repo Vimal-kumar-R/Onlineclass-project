fefc608b69e41b2a46713b372060cfa3ae10bf71
Vimal-kumar-R <143787874+Vimal-kumar-R@users.noreply.github.com>
2024-05-18 15:33:24 +0530

    Add files via upload

---
+++ frontend/crud/Jobs/Onlineedu.js
@@ -0,0 +1,129 @@
+import './Onlineedu.css'
+import './Onlineedu2.css'
+import './Onlineedu3.css'
+import './Onlineedu4.css'
+import './Onlineedu5.css'
+
+
+function Onlineedu() {
+
+
+
+    return (
+
+        <div>
+            <div id='cols'>
+                <div id="style1">
+                    <div id='style2'>
+                        <img className='img1' src='https://www-s3-live.kent.edu/s3fs-root/s3fs-public/styles/teaser_image/public/page/OnlineLearningStudentWeb.jpg?VersionId=5kur64I77M94Fo2ivo6dRngMscLfCkDN&h=c021a111&itok=TJ2fWxuL' width={250} height={250}></img>
+                        {/* <figcaption>Fullstack developer</figcaption> */}
+
+
+                        <div id='style3'><p>A full-stack developer is a developer or engineer who can build both the front end and the back end of a webssite</p></div>
+                        <div id='style6'><h3>Full stack developer</h3></div>
+                    </div>
+                    <div id='style5'>
+                        <a href='https://microcollege.in/'>readmore...</a>
+                    </div>
+
+
+                </div>
+                <div id='styless1'>
+                    <div id='styless2'>
+                        <img className='img2' src='https://etimg.etb2bimg.com/photo/94043764.cms' width={250} height={250}></img>
+                        <div id='styless3'> Professional showcases individuals' technical expertise in provisioning, operating, and managing distributed application systems on the AWS platform</div>
+                        <div id='styless6'><h3>Aws Devops Engineer</h3></div>
+                    </div>
+                    <div id='styless5'>
+                        <a href='#'>readmore....</a>
+
+                    </div>
+                </div>
+                <div id='styles1'>
+                    <div id='styles2'>
+                        <img className='img3' src='https://acerforeducation.acer.com/wp-content/uploads/2019/07/iStock-1035393946_02.jpg' width={250} height={250}></img>
+                        <div id='styles3'>Data analysis involves using statistical and computational techniques to derive insights from data such as healthcare, finance, marketing, and more.</div>
+                        <div id='styles6'><h3>Banck Ages</h3></div>
+                    </div>
+                    <div id='styles5'>
+                        <a href>readmore...</a>
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="container">
+                <svg viewBox="0 0 500 500" preserveAspectRatio="xMinYMin meet">
+                    <div>
+
+                    </div>
+
+                    {/* <image className='ani' href='https://img.freepik.com/premium-photo/funny-3d-cartoon-teenage-boy_183364-73453.jpg?w=740'></image> */}
+
+                    <path d="M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z" fill='rgb(38, 85, 69'></path>
+                    <text x="65" y="40" class="caption">Upskill for a dream
+                        job-ready you.</text>
+                </svg>
+
+            </div>
+
+            <div id='container1'>
+                <div id='containertex1'>
+                    {/* <div> */}
+                    {/* <h1>100% </h1><h1>job Assititance with Scholarship</h1> */}
+                    {/* <div><h3>EMI option</h3></div> */}
+
+                    {/* </div> */}
+                    <div class="container">
+                        <h2 class="title">
+                            <span class="title-word title-word-1">100%</span>
+                            <span class="title-word title-word-2">job Assistance</span><br></br>
+                            <span class="title-word title-word-3">Scholarship</span>
+                            <span class="title-word title-word-4"> Avail</span>
+                        </h2>
+                    </div>
+                    <div id='containerboxtop'>
+                        <h1>Today We are where</h1>
+                        <div id='containerbox'>
+                            <div id='boxpart1'>
+                                <div>
+                                <div id='box1'><h1>2+</h1><hr></hr><p>Years Wiswer</p></div>
+                                <div id='box2'><h1>1000+</h1><hr></hr><p>Curated Classes</p></div>
+                                </div>
+                            </div>
+                            <div id='boxpart2'>
+                                <div>
+                                <div id='box3'><h1>7000+</h1><hr></hr><p>export-instructors</p></div>
+                                <div id='box4'><h1>2000+</h1><hr></hr><p>Students storng</p></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id='containerimg1'>
+                    <img src='https://img.freepik.com/premium-photo/funny-3d-cartoon-teenage-boy_183364-73453.jpg?w=740' className='radio' ></img>
+                </div>
+            </div>
+            <div id='mark'>
+                <h1>OUR HIRING PARTNERS</h1>
+                <marquee><div>
+                    <img src='https://as2.ftcdn.net/v2/jpg/05/94/43/43/1000_F_594434334_z7TnpV9uIosBxdbPPAbNn9NDwUSqpRG7.jpg'></img>
+                    <img src='https://cdn.dribbble.com/userupload/3243652/file/original-8c2aa79266b35996637e9f2fe09b2d42.jpg?resize=320x240&vertical=center'></img>
+                  <img src='https://cdn.dribbble.com/userupload/14317311/file/original-d72760a9e1953e213b5203e7b8ebecce.jpg?resize=320x240&vertical=center'></img>
+                    </div></marquee>
+                    <marquee direction='right'><div>
+                        <img src='https://t3.ftcdn.net/jpg/04/57/01/22/240_F_457012261_NVjN9Hkxwqp8QlZlRO1repcfFlXUM1fN.jpg'></img>
+                        <img src='https://t3.ftcdn.net/jpg/02/58/91/48/240_F_258914870_3ojLH1enymdnTn4ZxKC7nfh7bXoPSoiM.jpg'></img>
+                        <img src='https://t3.ftcdn.net/jpg/06/89/20/16/240_F_689201670_c6mcQyGh8qUTPaOtUnBi69HBSJLl8VlG.jpg'></img>
+
+                        </div></marquee>          
+                    </div>
+
+        </div>
+
+
+
+    );
+}
+export default Onlineedu
